3bb6387288d600eaaa18bbfeeae41eba48327169
fmir <f.mir17@gmail.com>
2013-10-24 09:27:07 -0500

    Se agrego comentarios, y se quito codigo comentado.

--- CodeBreaker/src/co/edu/udea/GameCodeBreaker.java
+++ CodeBreaker/src/co/edu/udea/GameCodeBreaker.java
@@ -7,50 +7,16 @@ public class GameCodeBreaker {
 	{
 		String numeroEscondido ="1357";
 		String resultado = "";
-		/*
-		 * Refactor!!
-		 * Hecho para validar las dos primeras pruebas
-		 
-		if(valor.charAt(0)=='1'){
-			resultado +="X";
-		}
-		if(valor.charAt(1)=='3'){
-			resultado +="X";
-		}
-		if(valor.charAt(2)=='5'){
-			resultado +="X";
-		}
-		if(valor.charAt(3)=='7'){
-			resultado +="X";
-		}		
-		*/
 
-		 
-		/*Refactor
-		if(valor.charAt(0)==numeroEscondido.charAt(0)){
-			resultado +="X";
-		}
-		if(valor.charAt(1)==numeroEscondido.charAt(1)){
-			resultado +="X";
-		}
-		if(valor.charAt(2)==numeroEscondido.charAt(2)){
-			resultado +="X";
-		}
-		if(valor.charAt(3)==numeroEscondido.charAt(3)){
-			resultado +="X";
-		}
-		*/
-		
-		//Refactor
 		int i = 0;
 		int j= 0;
 		
-		
+		//Buscar Numeros que estan en la posicion exacta
 		while(i<4){
 			if(valor.charAt(i)==numeroEscondido.charAt(i)){
 				resultado +="X";
 			}
-			
+	
 			
 			i++;
 		}
@@ -74,20 +40,7 @@ public class GameCodeBreaker {
 			}
 			i++;
 		}
-		//Refactor para la 4 prueba
-		//Buscar el numero 1 desubicado
-		/*i=0;
-		while (i<4)
-		{
-			if(valor.charAt(i)=='1'){
-				
-				if(i!=0){
-					resultado += "_";
-				}
-											
-			}
-			i++;
-		}*/
+
 		return resultado;
 	}
 }
